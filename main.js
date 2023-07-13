8a2d525837971f88bd0ef876629dcd7097893ade
ldrennan279 <leedrennan279@gmail.com>
2023-07-13 07:25:32 +0100

    update 130723

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import {foodData} from "/data.js"
 
 const header = document.getElementById("header")
 const main = document.getElementById("main")
+const addBtn = document.getElementById("add-btn")
 
 header.innerHTML = `
     <div class="title">
@@ -10,7 +11,7 @@ header.innerHTML = `
     </div>
 `
 foodData.forEach((item) => {
-    main.innerHTML = `
+    main.innerHTML += `
         <section class="item-section" id="item-section">
                 <img src="${item.image}" alt="${item.item}" class="item-image" id="item-image">
                     <div class="item-div">
@@ -22,3 +23,29 @@ foodData.forEach((item) => {
             </section>
     `
 });
+
+
+document.addEventListener("click", ()=> {
+        console.log("clicked")
+        main.innerHTML += `
+        <section class="order-section" id="order-section">
+            <div class="order-section-header" id="order-section-header">
+                <h3>Your order</h3>
+            </div>
+            <div class="addedItems" id="addedItems">
+                <div class="added-item-remove" id="added-item-remove">
+                    <h3>Pizza</h3>
+                    <p class="remove-btn" id="remove-btn">remove</p>
+                </div>
+                <h3>$14</h3>
+            </div>
+            <div class="order-total" id="order-total">
+                <h3>Total price:</h3>
+                <h3>$14</h3> 
+            </div>
+            <button class="complete-order-btn" id="complete-order-btn">
+                    Complete order
+            </button>          
+       </section>
+        `
+})
